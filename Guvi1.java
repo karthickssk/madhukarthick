e82c43760167322c2d87a753293a360cec78cc92
karthickssk <40834149+karthickssk@users.noreply.github.com>
2018-07-04 09:00:13 -0700

    Create Guvi1.java

---
+++ Guvi1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Guvi1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==0)
+        {
+            System.out.print("zero");
+        }
+        else if(n>0)
+        {
+            System.out.print("positive");
+        }
+        else
+        System.out.print("negative");
+    }
+}
