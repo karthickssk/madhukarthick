c013e584df6468d35e0c807faddb43643241608f
karthickssk <40834149+karthickssk@users.noreply.github.com>
2018-07-10 15:09:08 +0530

    Update Guvi2.java

--- Guvi2.java
+++ Guvi2.java
@@ -5,12 +5,20 @@ class Guvi2
     {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        if(n%2==0&&n!=0)
+        if(n>0)
         {
+          if(n%2==0)
+           {
             System.out.print("even");
+           }
+          else
+          {
+           System.out.print("odd");
+          }
         }
-        else if(n!=0)
-        System.out.print("odd");
-    }
+        else
+        {
+            System.out.print("Invalid");
+        }
 }
-
+}
