879a29f8213cceabe3ce580d0e6ee8da69a5a06c
karthickssk <40834149+karthickssk@users.noreply.github.com>
2018-07-12 23:34:03 +0530

    Create Guvi4.java

---
+++ Guvi4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Guvi4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char k=sc.next().charAt(0);
+        if(k>='a' && k<='z')
+        {
+            System.out.print("Yes");
+        }
+        else
+        {
+            System.out.print("No");
+        }
+        }
+    }
+
