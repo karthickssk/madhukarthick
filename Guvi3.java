014873df259c748a083e5cacbc5f3cde4b87588a
karthickssk <40834149+karthickssk@users.noreply.github.com>
2018-07-12 22:59:48 +0530

    Create Guvi3.java

---
+++ Guvi3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Guvi3
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        char k=s.next().charAt(0);
+        if(k>='a' && k<='z')
+        {
+            if(m=='a'|| m=='e'|| m=='i'|| m=='o'|| m=='u')
+            {
+                System.out.print("Vowel");
+            }
+            else
+            {
+                System.out.print("Consonant");
+            }
+        }
+        else
+        {
+            System.out.print("Invalid");
+        }
+            }
+            }
+        
+        
+    
+        
+
+
+
+
